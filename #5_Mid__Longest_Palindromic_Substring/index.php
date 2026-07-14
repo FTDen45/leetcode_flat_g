cc5692a8e04210bacc04aac244e3aa78a9416bf7
Denis Kolosov <ftforest640@gmail.com>
2026-07-14 18:14:44 +0700

    #5_Mid Longest Palindromic Substring my 2 version

--- #5_Mid__Longest_Palindromic_Substring/index.php
+++ #5_Mid__Longest_Palindromic_Substring/index.php
@@ -16,22 +16,23 @@ class Solution {
             if ($i > 0) $left = substr($s, $i - $k, 1);
             else $left = substr($s, $i, 1);
             if ($i < $len) $right = substr($s, $i + $k, 1);
-            else $right = "";
+            else $right = substr($s, $i, 1);
             $res_cur = substr($s, $i, 1);
             while ($left == $right) {
                 $res_cur = $left . $res_cur . $right;
                 $len_cur = 1 + $k * 2;
                 $k++;
-                if ($i > 0) $left = substr($s, $i - $k, 1);
-                else $left = substr($s, $i, 1);
-                if ($i < $len) $right = substr($s, $i + $k, 1);
-                else $right = "";
-
+                if (($i - $k) >= 0) $left = substr($s, $i - $k, 1);
+                else break;
+                if (($i + $k) < $len) $right = substr($s, $i + $k, 1);
+                else break;
             }
             if ($len_cur > $len_max) {
                 $len_max = $len_cur;
                 $res_max = $res_cur;
             }
+            $res_cur = "";
+            $k = 1;
         }
 
         return $res_max;
@@ -39,7 +40,7 @@ class Solution {
 }
 
 $str = "cbbd";
-$str = "babad";
+$str = "bcdad";
 
 $obj = new Solution();
 echo $obj->longestPalindrome($str);
